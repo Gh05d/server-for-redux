c878f1eafdb80d5d91a324965e746be014454ea6
Gh05d <pacal.clanget@googlemail.com>
2017-05-24 14:08:53 +0200

    Finished server for authentication

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -15,7 +15,7 @@ exports.signin = function(req, res, next) {
 }
 
 exports.signup = function(req, res, next) {
- const email = req.body.email.toLowerCase();
+ const email = req.body.email;
  const password = req.body.password;
 
  if (!email || !password) {
